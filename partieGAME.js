8556a6c3823159e12471fda48d5fbf502cdc7492
M1ck3y-ru <clement.garciaspro@gmail.com>
2023-06-17 17:43:24 +0200

    partiPArtieGAME

--- partieGAME.js
+++ partieGAME.js
@@ -26,3 +26,51 @@ console.log("Les joueurs sélectionnés sont :");
 selectedPlayers.forEach((player, index) => {
   console.log(`Joueur ${index + 1}: ${player}`);
 });
+
+function jouerPartie(player) {
+  const scores = {};
+  player.forEach((player) => {
+    scores[player] = [];
+  });
+
+  for (let frame = 1; frame <= 10; frame++) {
+    console.log(`Frame ${frame}`);
+    for (let i = 0; i < joueurs.length; i++) {
+      console.log(`Joueur: ${joueurs[i]}`);
+      const lancer1 = demanderNombreQuilles(joueurs[i], 1);
+      scores[joueurs[i]].push(lancer1);
+      if (lancer1 === 10) {
+        console.log("Strike !");
+        continue;
+      }
+      const lancer2 = demanderNombreQuilles(joueurs[i], 2);
+      scores[joueurs[i]].push(lancer2);
+      if (lancer1 + lancer2 === 10) {
+        console.log("Spare !");
+      }
+    }
+  }
+
+  return scores;
+}
+
+function demanderNombreQuilles(joueur, lancer) {
+  let nombreQuilles;
+  do {
+    nombreQuilles = parseInt(prompt(`Joueur ${joueur}, Entrez le nombre de quilles renversées lors du lancer ${lancer}: `));
+    if (isNaN(nombreQuilles) || nombreQuilles < 0 || nombreQuilles > 10) {
+      console.log("Veuillez entrer un nombre valide entre 0 et 10.");
+    }
+  } while (isNaN(nombreQuilles) || nombreQuilles < 0 || nombreQuilles > 10);
+
+  return nombreQuilles;
+}
+
+// Test du déroulement de la partie
+const joueurs = [players, players];
+const scores = jouerPartie(joueurs);
+
+console.log("Scores :");
+for (const joueur in scores) {
+  console.log(`${joueur} : ${scores[joueur].join(", ")}`);
+}
